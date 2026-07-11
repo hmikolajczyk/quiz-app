bdb4c1e7b23968ecc13822ff9f8b08e1d108fd84
dianthusie <haniamikola@gmail.com>
2026-07-11 21:34:49 +0200

    feat: implement getCurrentQuestion with bounds checking

--- src/main/java/quizapp/service/Quiz.java
+++ src/main/java/quizapp/service/Quiz.java
@@ -4,14 +4,23 @@ import quizapp.model.Question;
 
 public class Quiz {
     private final List<Question> questionList;
+    private int questionIndex;
     private int userPoints;
 
     public Quiz(List<Question> questionList) {
         if(questionList == null || questionList.isEmpty()) {
             throw new IllegalArgumentException("Lista pytań nie może być pusta ani nullem!");
         }
-        
+
         this.questionList = questionList;
+        questionIndex = 0;
         userPoints = 0;
+    }
+
+    public Question getCurrentQuestion() {
+        if(questionIndex < 0 || questionIndex >= questionList.size()) {
+            throw new IndexOutOfBoundsException("Skończyły się pytania!");
+        }
+        return questionList.get(questionIndex);
     }
 }
