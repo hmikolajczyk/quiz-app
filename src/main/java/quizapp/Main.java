aa5b073b02c9efd3b4f0156829e7a1f04980962c
dianthusie <haniamikola@gmail.com>
2026-07-12 02:00:15 +0200

    feat: add user input conversion and answer validation

--- src/main/java/quizapp/Main.java
+++ src/main/java/quizapp/Main.java
@@ -11,7 +11,7 @@ public class Main {
     public static void main(String[] args) {
         Question question1 = new Question(QuestionType.SINGLE_CHOICE, "Treść pytania nr 1", List.of("A: Opcja A", "B: Opcja B", "C: Opcja C"), List.of(0));
 
-        Question question2 = new Question(QuestionType.SINGLE_CHOICE, "Treść pytania nr 2", List.of("A: Opcja A", "B: Opcja B", "C: Opcja C"), List.of(1));
+        Question question2 = new Question(QuestionType.SINGLE_CHOICE, "Treść pytania nr 2", List.of("A: Opcja A", "B: Opcja B", "C: Opcja C"), List.of(1, 2));
 
         List<Question> questions = List.of(question1, question2);
         Quiz quizTest = new Quiz(questions);
@@ -20,16 +20,26 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
-        String userAnswer = scanner.nextLine().toLowerCase();
-        String[] splitAnswers = userAnswer.split(" ");
-        List<Integer> answerIndexes = new ArrayList<>();
+        String userAnswer = scanner.nextLine();
+        List<Integer> answerIndices = convertUserLettersToIndices(userAnswer);
+
+        if(quizTest.isAnswerCorrect(answerIndices)) {
+            System.out.println("Poprawne odpowiedzi.");
+        } else {
+            System.out.println("Błędne odpowiedzi.");
+        }
+    }
+
+    private static List<Integer> convertUserLettersToIndices(String input) {
+        String[] splitAnswers = input.toLowerCase().split(" ");
+        List<Integer> answerIndices = new ArrayList<>();
 
         for(String answer : splitAnswers) {
             char letter = answer.charAt(0);
             int index = letter - 'a';
-            answerIndexes.add(index);
+            answerIndices.add(index);
         }
 
-        System.out.println("Indeksy: " + answerIndexes);
+        return answerIndices;
     }
 }
